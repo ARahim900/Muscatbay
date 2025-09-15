7f7d451ddcd3a93e636b4e1d9dfac89c67da169c
Abdularahim Al Balushi <143422547+ARahim900@users.noreply.github.com>
2025-09-15 23:23:35 +0400

    API

---
+++ entities.js
@@ -0,0 +1,31 @@
+import { base44 } from './base44Client';
+
+
+export const System = base44.entities.System;
+
+export const Alert = base44.entities.Alert;
+
+export const Contractor = base44.entities.Contractor;
+
+export const Meter = base44.entities.Meter;
+
+export const Contract = base44.entities.Contract;
+
+export const STPOperation = base44.entities.STPOperation;
+
+export const WaterMeter = base44.entities.WaterMeter;
+
+export const FireSafetyEquipment = base44.entities.FireSafetyEquipment;
+
+export const HvacMaintenanceLog = base44.entities.HvacMaintenanceLog;
+
+export const DailyWaterReading = base44.entities.DailyWaterReading;
+
+export const MaintenanceSchedule = base44.entities.MaintenanceSchedule;
+
+export const MaintenanceHistory = base44.entities.MaintenanceHistory;
+
+
+
+// auth sdk:
+export const User = base44.auth;
